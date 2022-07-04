e1f680fb99cf0dadd5bcb33340236c80265b6af0
David Guilherme <63566701+david042@users.noreply.github.com>
2022-07-03 21:59:01 -0300

    Add files via upload

---
+++ bolero.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+C4=262
+D4=294
+E4=330
+F4=349
+G4=392
+A4=440
+B4=494
+C5=523
+D5=587
+
+beep -f $C5 -l 1500 -n -f $B4 -l 250 -n -f $C5 -l 250 -n -f $D5 -l 250 -n -f $C5 -l 250 -n -f $B4 -l 250 -n -f $A4 -l 250 -n -f $C5 -l 500 -n -f $C5 -l 250 -n -f $A4 -l 250 -n -f $C5 -l 1500 -n -f $B4 -l 250 -n -f $C5 -l 250 -n -f $A4 -l 250 -n -f $G4 -l 250 -n -f $E4 -l 250 -n -f $F4 -l 250 -n -f $G4 -l 2250 -n -f $F4 -l 250 -n -f $E4 -l 250 -n -f $D4 -l 250 -n -f $E4 -l 250 -n -f $F4 -l 250 -n -f $G4 -l 250 -n -f $A4 -l 250 -n -f $G4 -l 2250 -n -f $A4 -l 250 -n -f $B4 -l 250 -n -f $A4 -l 250 -n -f $G4 -l 250 -n -f $F4 -l 250 -n -f $E4 -l 250 -n -f $D4 -l 250 -n -f $E4 -l 250 -n -f $D4 -l 250 -n -f $C4 -l 1000 -n -f $C4 -l 250 -n -f $D4 -l 250 -n -f $E4 -l 375
+sleep 0.125
+beep -f $F4 -l 375
+sleep 0.125
+beep -f $D4 -l 1000 -n -f $G4 -l 4500
+sleep 0.5
+beep -f $D5 -l 1750 -n -f $C5 -l 250 -n -f $B4 -l 250 -n -f $A4 -l 250 -n -f $B4 -l 250 -n -f $C5 -l 250 -n -f $D5 -l 250 -n -f $C5 -l 250 -n -f $B4 -l 750 -n -f $C5 -l 250 -n -f $B4 -l 250 -n -f $A4 -l 250 -n -f $C5 -l 250 -n -f $B4 -l 250 -n -f $A4 -l 250 -n  -f $F4 -l 1000 -n -f $F4 -l 125
+sleep 0.125
+beep -f $F4 -l 125
+sleep 0.125
+beep -f $F4 -l 250
+sleep 0.25
+beep -f $A4 -l 250
+sleep 0.25
+beep -f $C5 -l 250 -n -f $A4 -l 250 -n -f $B4 -l 250 -n -f $G4 -l 250 -n -f $F4 -l 500 -n -f $F4 -l 125
+sleep 0.125
+beep -f $F4 -l 125
+sleep 0.125
+beep -f $F4 -l 250
+sleep 0.25
+beep -f $A4 -l 250
+sleep 0.25
+beep -f $B4 -l 250 -n -f $G4 -l 250 -n -f $A4 -l 250 -n -f $F4 -l 250 -n -f $D4 -l 500 -n -f $D4 -l 250 -n -f $C4 -l 250 -n -f $D4 -l 1500 -n -f $D4 -l 125
+sleep 0.125
+beep -f $D4 -l 125
+sleep 0.125
+beep -f $D4 -l 250
+sleep 0.25
+beep -f $F4 -l 250
+sleep 0.25
+beep -f $A4 -l 250 -n -f $F4 -l 250 -n -f $G4 -l 250 -n -f $E4 -l 250 -n -f $D4 -l 500 -n -f $D4 -l 250 -n -f $C4 -l 250 -n -f $D4 -l 1500 -n -f $D4 -l 250 -n -f $C4 -l 250 -n -f $D4 -l 500 -n -f $E4 -l 250 -n -f $F4 -l 250 -n -f $G4 -l 2250 -n -f $F4 -l 250 -n -f $E4 -l 250 -n -f $D4 -l 250 -n -f $C4 -l 500
+
+
+
+
+
+
